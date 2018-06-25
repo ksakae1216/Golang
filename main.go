ff2164c884cc2f3282551ef4030d0c4977cfc458
榮　高太郎 <sakae@sakaetakatarounoMacBook-puro.local>
2018-06-25 23:14:39 +0900

    second commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"./funcpkg"
+)
+
+func main() {
+
+	nyuumonFunc() // 入門編
+	// funcpkg.IfFunc() // if文
+	// funcpkg.SwitchFunc("Java") // switch文
+	// funcpkg.ForFunc() // for文
+	// funcpkg.PointerFunc() // ポインタ
+	// funcpkg.ReceiverFunc() // レシーバー
+	// funcpkg.StructFunc() // 構造体
+	funcpkg.AboutFunc() // 関数いろいろ
+}
+
+// 入門編
+// ポイント
+// 変数の宣言は型(string, intなど)を最後に持ってくる
+func nyuumonFunc() {
+	var str string     // 型を宣言
+	str = "Start!!!!!" // 値を設定
+
+	fmt.Println("Hellow Go World!! -> " + str)
+}
