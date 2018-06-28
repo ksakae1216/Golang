8bdcad9e3998551c036ab000199a4f6190cedc71
ksakae1216 <ksakae1216@gmail.com>
2018-06-28 11:08:10 +0900

    Update SliceFunc.go
    「要素追加」、「コピー」を追加

--- funcpkg/SliceFunc.go
+++ funcpkg/SliceFunc.go
@@ -10,14 +10,24 @@ import (
 // 配列と似ているが値を参照型で保持している。
 // スライス式"[:]"を使えばスライスになる
 func SliceFunc() {
-	val := [...]string{"A", "B", "C", "D"}
-
-	fmt.Println("変更前 -> ", val)
+	val := []string{"A", "B", "C", "D"}
+	fmt.Println("変更前　 -> ", val)
 
 	// スライスで関数に渡す（参照型なので値を変更できる)
 	changeVal(val[:])
+	fmt.Println("変更後　 -> ", val)
 
-	fmt.Println("変更後 -> ", val)
+	// スライスに要素を追加
+	val = append(val[:], "E", "F")
+	fmt.Println("追加後　 -> ", val)
+
+	// スライス要素のコピー
+	copy(val[:], []string{"A", "B"})
+	fmt.Println("コピー後 -> ", val)
+
+	// スライス要素に途中からコピー
+	copy(val[2:], []string{"C", "D"})
+	fmt.Println("コピー後 -> ", val)
 }
 
 func changeVal(valList []string) {
